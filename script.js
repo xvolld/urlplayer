93913788647e154f30d9843ecc7174a31efc368d
xvolld <136752887+xvolld@users.noreply.github.com>
2024-05-30 13:20:27 +0330

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,11 @@ function playMedia() {
     // Initialize Plyr video player
     var videoPlayer = new Plyr('#videoPlayer', {
         controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen', 'settings', 'speed'], // Add controls including speed
-        settings: ['speed']
+        settings: ['speed', 'quality'], // Add quality settings
+        quality: { // Add quality options
+            default: '720p',
+            options: ['240p', '360p', '480p', '720p', '1080p']
+        }
     });
 
     // Get speed indicator element
@@ -46,4 +50,48 @@ function playMedia() {
     videoPlayer.elements.container.addEventListener('touchstart', handleLongPressStart);
     videoPlayer.elements.container.addEventListener('touchend', handleLongPressEnd);
     videoPlayer.elements.container.addEventListener('touchcancel', handleLongPressEnd);
+
+    // Function to handle swipe left and right
+    videoPlayer.elements.container.addEventListener('touchstart', handleTouchStart, false);
+    videoPlayer.elements.container.addEventListener('touchmove', handleTouchMove, false);
+
+    var xDown = null;
+    var yDown = null;
+
+    function handleTouchStart(evt) {
+        xDown = evt.touches[0].clientX;
+        yDown = evt.touches[0].clientY;
+    }
+    
+
+    function handleTouchMove(evt) {
+        if (!xDown || !yDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) { // Most significant.
+            if (xDiff > 0) {
+                /* left swipe */
+                videoPlayer.currentTime -= 10; // Go back 10 seconds
+            } else {
+                /* right swipe */
+                videoPlayer.currentTime += 10; // Go forward 10 seconds
+            }
+        }
+
+        // Reset values
+        xDown = null;
+        yDown = null;
+    }
+
+    // Prevent full screen on double click
+    videoPlayer.elements.container.ondblclick = function(e) {
+        e.preventDefault();
+    };
 }
